83299cbb368252f18741ffb5e4c902333afb4ff5
jocklawrie <jock.lawrie@gmail.com>
2022-01-07 17:21:24 +1100

    Refactored.

--- src/GenerateSysimage.jl
+++ src/GenerateSysimage.jl
@@ -8,20 +8,29 @@ using Dates
 using Pkg
 using PackageCompiler
 using Logging
+using TOML
 
 function generate_sysimage(packagelist::Vector{String}, sysimage_name::String, precompile_script)
     @info "$(now()) Start"
     @info "$(now()) Determining the path of the resulting sysimage"
     outdir = joinpath(pwd(), "output")
     !isdir(outdir) && mkdir(outdir)
+    create_new_depot(outdir)  # Fresh depot to be used by the temporary project
     result_fullpath = joinpath(outdir, sysimage_name)
-
+    
     @info "$(now()) Generating temporary project"
-    precompile_script = generate_precompile_script(packagelist, sysimage_name, precompile_script, outdir)
-    tempproject_dir   = generate_tempproject(packagelist)
+    tempproject_dir = generate_tempproject(packagelist, outdir)
+    if precompile_script == "usetests"
+        @info "$(now()) Generating precompile script from the test suites of listed packages"
+        precompile_script = generate_precompile_script(packagelist, sysimage_name, outdir, tempproject_dir)
+    end
 
     @info "$(now()) Creating sysimage from temporary project"
-    create_sysimage(packagelist; sysimage_path=result_fullpath, precompile_execution_file=precompile_script)
+    if isnothing(precompile_script)
+        create_sysimage(packagelist; sysimage_path=result_fullpath)
+    else
+        create_sysimage(packagelist; sysimage_path=result_fullpath, precompile_execution_file=precompile_script)
+    end
     @info "$(now()) Done. The new sysimage is at: $(result_fullpath)"
 
     @info "$(now()) Removing temporary project"
@@ -32,45 +41,113 @@ function generate_sysimage(packagelist::Vector{String}, sysimage_name::String, p
 end
 
 ################################################################################
-# Generate precompile script
+# Generate temp project
 
-function generate_precompile_script(packagelist, sysimage_name, precompile_script, outdir)
-    isnothing(precompile_script)    && return generate_precompile_script_from_packagelist(packagelist, outdir, sysimage_name, false)
-    precompile_script == "usetests" && return generate_precompile_script_from_packagelist(packagelist, outdir, sysimage_name, true)
-    isfile(precompile_script)       && return precompile_script
-    error("The precompile script is unrecognised")
+function create_new_depot(outdir)
+    newdepot = joinpath(outdir, ".julia")
+    isdir(newdepot) && rm(newdepot; recursive=true)
+    mkdir(newdepot)
+    empty!(DEPOT_PATH)
+    push!(DEPOT_PATH, newdepot)
 end
 
-"""
-For each package in packagelist:
-- Include a line containing: using packagename
-- If usetests == true, also include a line that runs the package's test suite.
-"""
-function generate_precompile_script_from_packagelist(packagelist, outdir, sysimage_name, usetests::Bool)
-    @info "$(now()) Auto-generating precompile script"
-    imagename, ext  = splitext(sysimage_name)
-    precompile_file = joinpath(outdir, "$(imagename).jl")
-    open(precompile_file, "w") do f
-        for p in packagelist
-            write(f, "using $(p)\n")
-            usetests && write(f, "include(joinpath(pkgdir($(p)), \"test\", \"runtests.jl\"))\n")
-        end
-    end
-    precompile_file
-end
-
-################################################################################
-# Other functions
-
-function generate_tempproject(packagelist)
+function generate_tempproject(packagelist, outdir)
+    # Init tempproject
     cd(tempdir())
+    isdir("tempproject") && rm("tempproject"; recursive=true)
     Pkg.generate("tempproject")
     cd("tempproject")
     Pkg.activate(".")
+    pkgdir = pwd()
+
+    # Add packages to tempproject
     for p in packagelist
         Pkg.add(p)
     end
     pwd()
+end
+
+################################################################################
+# Generate precompile script
+
+function generate_precompile_script(packagelist, sysimage_name, outdir, tempproject_dir)
+    precompile_script = init_precompilescript(sysimage_name, outdir)
+    append_package_imports_to_precompilescript!(precompile_script, packagelist)
+    pkgname2pkgdir = construct_pkgname2pkgdir()
+    extrapackages  = construct_extrapackages(packagelist, pkgname2pkgdir)
+    add_packages_to_tempproject!(tempproject_dir, extrapackages)
+    append_package_imports_to_precompilescript!(precompile_script, extrapackages)
+    append_tests_to_precompilescript!(precompile_script, packagelist)
+end
+
+function init_precompilescript(sysimage_name, outdir)
+    imagename, ext    = splitext(sysimage_name)
+    precompile_script = joinpath(outdir, "$(imagename).jl")
+    touch(precompile_script)
+end
+
+function append_package_imports_to_precompilescript!(precompile_script, packagelist)
+    open(precompile_script, "w") do f
+        for p in packagelist  # Import all dependencies before running tests
+            write(f, "using $(p)\n")
+        end
+    end
+end
+
+function construct_pkgname2pkgdir()
+    result = Dict{String, String}()  # packagename => packagedir
+    deps   = Pkg.dependencies()
+    for (uuid, pkginfo) in deps
+        result[pkginfo.name] = pkginfo.source
+    end
+    result
+end
+
+function construct_extrapackages(packagelist, pkgname2pkgdir)
+    result = Set{String}()
+    packages_done = Set(packagelist)
+    for pkgname in packagelist
+        packagedir     = pkgname2pkgdir[pkgname]
+        extra_packages = get_extra_packages(result, packagedir)
+        for p in extrapackages
+            in(p, packages_done) && continue
+            push!(result, p)
+            push!(packages_done, p)
+        end
+    end
+    sort!([x for x in result])
+end
+
+function get_extra_packages(result, packagedir::String)
+    projtoml = joinpath(packagedir, "Project.toml")
+    extract_package_names!(result, projtoml, "extras")
+    projtoml = joinpath(packagedir, "test", "Project.toml")
+    extract_package_names!(result, projtoml, "deps")
+    result
+end
+
+"Insert into result the packages listed in the specified section of the Project.toml file."
+function extract_package_names!(result::Set{String}, projtoml_fullpath, section)
+    !isfile(projtoml_fullpath) && return
+    d = TOML.parsefile(projtoml_fullpath)
+    !haskey(d, section) && return
+    for (packagename, uuid) in d[section]
+        push!(result, packagename)
+    end
+end
+
+function add_packages_to_tempproject!(tempproject_dir, extrapackages)
+    for p in extrapackages
+        Pkg.add(p)
+    end
+end
+
+function append_tests_to_precompilescript!(precompile_script, packagelist, pkgname2pkgdir)
+    open(precompile_script, "w") do f
+        for pkgname in packagelist
+            write(f, "include($(pkgname2pkgdir[pkgname]), \"test\", \"runtests.jl\"))\n")
+        end
+    end
 end
 
 end
